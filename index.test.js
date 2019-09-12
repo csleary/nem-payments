af0c57449f9a0bfe41902b74cc8656e5ac122b56
Chris <csleary@gmail.com>
2019-09-12 21:32:05 +0200

    Some jsdoc annotations, more tests.

--- index.test.js
+++ index.test.js
@@ -13,3 +13,26 @@ test('fetches a \'hello world!\' live nem transaction', async () => {
     nemNode: 'http://173.212.238.176'
   });
 });
+
+test('uses a default unspecified node', async () => {
+  expect(
+    await nemPayments(
+      'NAER66-DXCNYE-BNMTWA-PKG7CU-27CMUP-TQQDSM-2KL6',
+      'Hello World!'
+    )
+  ).toMatchObject({
+    transactions: expect.any(Array),
+    xemPaid: 0.1,
+    nemNode: expect.any(String)
+  });
+});
+
+test('search for transactions with empty messages', async () => {
+  const payments = await nemPayments('NAER66-DXCNYE-BNMTWA-PKG7CU-27CMUP-TQQDSM-2KL6', '');
+
+  expect(payments).toMatchObject({
+    transactions: expect.any(Array),
+    xemPaid: expect.any(Number),
+    nemNode: expect.any(String)
+  });
+});
